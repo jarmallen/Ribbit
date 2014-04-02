a6528bff17c320794f3983cbc14f18923c25bafd
jarmallen <jallen@jallen-mba.local>
2014-04-01 21:25:49 -0700

    Initial Commit

---
+++ Ribbit/JAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JAAppDelegate.h
+//  Ribbit
+//
+//  Created by Jared Allen on 4/1/14.
+//  Copyright (c) 2014 Jared Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Jared Allen on 4/1/14.
+//  Copyright (c) 2014 Jared Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JAAppDelegate class]));
+    }
+}
